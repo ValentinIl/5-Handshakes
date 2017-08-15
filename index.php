cfc4fd4dc863f23a6cfeca9fc8f0c6f7f64287b5
ValentinIl <valentin.ilinov@beetroot.se>
2017-08-15 18:06:57 +0300

    update index

--- index.php
+++ index.php
@@ -2,105 +2,84 @@
 session_start();
 require_once __DIR__ . '/src/Facebook/autoload.php';
 
-$fb = new Facebook\Facebook([
-	'app_id' => '433400397053452',
-	'app_secret' => '4ffe61f16eaa6f30b3d8dee98ade1eda',
+$fb = new Facebook\Facebook( [
+	'app_id'                => '433400397053452',
+	'app_secret'            => '4ffe61f16eaa6f30b3d8dee98ade1eda',
 	'default_graph_version' => 'v2.10',
-]);
+] );
 
-$helper = $fb->getCanvasHelper();
+$helper = $fb->getRedirectLoginHelper();
 
-$permissions = ['user_friends']; // optionnal
+$permissions = [ 'email' ]; // optional
 
 try {
-	if (isset($_SESSION['facebook_access_token'])) {
+	if ( isset( $_SESSION['facebook_access_token'] ) ) {
 		$accessToken = $_SESSION['facebook_access_token'];
 	} else {
 		$accessToken = $helper->getAccessToken();
 	}
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
+} catch ( Facebook\Exceptions\FacebookResponseException $e ) {
 	// When Graph returns an error
 	echo 'Graph returned an error: ' . $e->getMessage();
+
 	exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
+} catch ( Facebook\Exceptions\FacebookSDKException $e ) {
 	// When validation fails or other local issues
 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
 	exit;
 }
 
-if (isset($accessToken)) {
-	if (isset($_SESSION['facebook_access_token'])) {
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+if ( isset( $accessToken ) ) {
+	if ( isset( $_SESSION['facebook_access_token'] ) ) {
+		$fb->setDefaultAccessToken( $_SESSION['facebook_access_token'] );
 	} else {
+		// getting short-lived access token
 		$_SESSION['facebook_access_token'] = (string) $accessToken;
 
 		// OAuth 2.0 client handler
 		$oAuth2Client = $fb->getOAuth2Client();
 
 		// Exchanges a short-lived access token for a long-lived one
-		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+		$longLivedAccessToken
+			= $oAuth2Client->getLongLivedAccessToken( $_SESSION['facebook_access_token'] );
 
 		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
 
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+		// setting default access token to be used in script
+		$fb->setDefaultAccessToken( $_SESSION['facebook_access_token'] );
 	}
 
-	// validating the access token
-	try {
-		$request = $fb->get('/me');
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		if ($e->getCode() == 190) {
-			unset($_SESSION['facebook_access_token']);
-			$helper = $fb->getRedirectLoginHelper();
-			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
-			echo "<script>window.top.location.href='".$loginUrl."'</script>";
-		}
-		exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
+	// redirect the user back to the same page if it has "code" GET variable
+	if ( isset( $_GET['code'] ) ) {
+		header( 'Location: ./' );
 	}
 
-	// get list of friends' names
+	// getting basic info about user
 	try {
-		$requestFriends = $fb->get('/me/taggable_friends?fields=name&limit=100');
-		$friends = $requestFriends->getGraphEdge();
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		$profile_request
+			     = $fb->get( '/me?fields=name,first_name,last_name,email' );
+		$profile = $profile_request->getGraphNode()->asArray();
+	} catch ( Facebook\Exceptions\FacebookResponseException $e ) {
 		// When Graph returns an error
 		echo 'Graph returned an error: ' . $e->getMessage();
+		session_destroy();
+		// redirecting user back to app login page
+		header( "Location: ./" );
 		exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	} catch ( Facebook\Exceptions\FacebookSDKException $e ) {
 		// When validation fails or other local issues
 		echo 'Facebook SDK returned an error: ' . $e->getMessage();
 		exit;
 	}
 
-	// if have more friends than 100 as we defined the limit above on line no. 68
-	if ($fb->next($friends)) {
-		$allFriends = array();
-		$friendsArray = $friends->asArray();
-		$allFriends = array_merge($friendsArray, $allFriends);
-		while ($friends = $fb->next($friends)) {
-			$friendsArray = $friends->asArray();
-			$allFriends = array_merge($friendsArray, $allFriends);
-		}
-		foreach ($allFriends as $key) {
-			echo $key['name'] . "<br>";
-		}
-		echo count($allFriends);
-	} else {
-		$allFriends = $friends->asArray();
-		$totalFriends = count($allFriends);
-		foreach ($allFriends as $key) {
-			echo $key['name'] . "<br>";
-		}
-	}
+	// printing $profile array on the screen which holds the basic info about user
+	print_r( $profile );
 
 	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
-	$helper = $fb->getRedirectLoginHelper();
-	$loginUrl = $helper->getLoginUrl('https://valentinil.github.io/5-Handshakes', $permissions);
-	echo "<script>window.top.location.href='".$loginUrl."'</script>";
+	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
+	$loginUrl
+		= $helper->getLoginUrl( 'http://valentinil.github.io/5-Handshakes/',
+		$permissions );
+	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
